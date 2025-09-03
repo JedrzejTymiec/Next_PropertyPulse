3d99599220c11369a7ebe8e0ad83f41169191191
Jedrzej Tymiec <jedrek1692@gmail.com>
2025-09-03 15:52:35 +0200

    feat: aside html

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -8,6 +8,9 @@ import { paths } from '@/constants/paths';
 import { FaArrowLeft } from 'react-icons/fa';
 import { PropertyDetails } from '@/components/PropertyDetails/PropertyDetails';
 import { Gallery } from '@/components/PropertyDetails/components/Gallery';
+import { BookmarkButton } from '@/components/BookmarkButton';
+import { ShareButton } from '@/components/ShareButton';
+import { ContactForm } from '@/components/ContactForm';
 
 interface Params {
   id: string;
@@ -46,6 +49,11 @@ const PropertyPage = async ({ params: { id } }: PropertyPageProps) => {
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
+            <aside className="space-y-4">
+              <BookmarkButton />
+              <ShareButton />
+              <ContactForm />
+            </aside>
           </div>
         </div>
       </section>
