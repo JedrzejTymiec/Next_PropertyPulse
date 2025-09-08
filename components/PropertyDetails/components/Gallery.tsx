4c24af50234e5f868e5fdb471a9bd32809cdbc56
Jedrzej Tymiec <jedrek1692@gmail.com>
2025-09-08 17:37:36 +0200

    feat: swipe gallery

--- components/PropertyDetails/components/Gallery.tsx
+++ components/PropertyDetails/components/Gallery.tsx
@@ -1,5 +1,8 @@
+'use client';
 import { isOddAndLast } from '@/utils/isOddAndLast';
 import Image from 'next/image';
+import 'photoswipe/dist/photoswipe.css';
+import { Gallery as PHGallery, Item } from 'react-photoswipe-gallery';
 
 interface GalleryProps {
   images: string[];
@@ -9,40 +12,64 @@ interface GalleryProps {
 
 export const Gallery = ({ images }: GalleryProps) => {
   return (
-    <section className="bg-blue-50 p-4">
-      <div className="container mx-auto">
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt=""
-            className="object-cover h-[400px] mx-auto rounded-xl"
-            width={1800}
-            height={400}
-            priority={true}
-          />
-        ) : (
-          <div className="grid grid-cols-2 gap-4">
-            {images.map((image, index) => (
-              <div
-                key={image}
-                className={
-                  isOddAndLast(index, images.length)
-                    ? 'col-span-2'
-                    : 'col-span-1'
-                }
-              >
+    <PHGallery>
+      <section className="bg-blue-50 p-4">
+        <div className="container mx-auto">
+          {images.length === 1 ? (
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width="1000"
+              height="600"
+            >
+              {({ ref, open }) => (
                 <Image
-                  src={image}
+                  src={images[0]}
+                  ref={ref}
+                  onClick={open}
                   alt=""
-                  className="object-cover h-[400px] w-full rounded-xl"
+                  className="object-cover h-[400px] mx-auto rounded-xl cursor-pointer"
                   width={1800}
                   height={400}
+                  priority={true}
                 />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+              )}
+            </Item>
+          ) : (
+            <div className="grid grid-cols-2 gap-4">
+              {images.map((image, index) => (
+                <div
+                  key={image}
+                  className={
+                    isOddAndLast(index, images.length)
+                      ? 'col-span-2'
+                      : 'col-span-1'
+                  }
+                >
+                  <Item
+                    original={image}
+                    thumbnail={image}
+                    width="1280"
+                    height="720"
+                  >
+                    {({ ref, open }) => (
+                      <Image
+                        src={image}
+                        ref={ref}
+                        onClick={open}
+                        alt=""
+                        className="object-cover h-[400px] w-full rounded-xl cursor-pointer"
+                        width={1800}
+                        height={400}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </PHGallery>
   );
 };
