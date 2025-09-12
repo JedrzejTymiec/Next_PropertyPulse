c2a9c56e104376d153bf503287999682e2144757
Jedrzej Tymiec <jedrek1692@gmail.com>
2025-09-12 22:11:38 +0200

    fix: contact form, bookmark button hidden for property owners

--- components/BookmarkButton.tsx
+++ components/BookmarkButton.tsx
@@ -8,14 +8,14 @@ import { isBookmarked as checkIfBookmarked } from '@/app/actions/Property/isBook
 
 interface BookmarkButtonProps {
   id: string;
+  ownerId: string;
 }
 
-export const BookmarkButton = ({ id }: BookmarkButtonProps) => {
+export const BookmarkButton = ({ id, ownerId }: BookmarkButtonProps) => {
   const [isBookmarked, setIsBookmarked] = useState<boolean>(false);
-  const { isAuthenticated } = useIsAuthenticated();
-  const color = isBookmarked
-    ? 'bg-green-500 hover:bg-green-600'
-    : 'bg-blue-500 hover:bg-blue-600';
+  const { isAuthenticated, userId } = useIsAuthenticated();
+  const isVisible = isAuthenticated && userId !== ownerId;
+  const color = isBookmarked ? 'bg-green-500 hover:bg-green-600' : 'bg-blue-500 hover:bg-blue-600';
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -30,14 +30,14 @@ export const BookmarkButton = ({ id }: BookmarkButtonProps) => {
   const onClick = useCallback(async () => {
     try {
       const message = await bookmarkProperty(id);
-      setIsBookmarked((prev) => !prev);
+      setIsBookmarked(prev => !prev);
       toast.success(message);
     } catch (e) {
       toast.error(e as string);
     }
   }, [id]);
 
-  if (!isAuthenticated) {
+  if (!isVisible) {
     return null;
   }
 
@@ -46,8 +46,7 @@ export const BookmarkButton = ({ id }: BookmarkButtonProps) => {
       onClick={onClick}
       className={`${color} text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center`}
     >
-      <FaBookmark className="mr-2" />{' '}
-      {isBookmarked ? 'Remove Bookmark' : 'Bookmark Property'}
+      <FaBookmark className="mr-2" /> {isBookmarked ? 'Remove Bookmark' : 'Bookmark Property'}
     </button>
   );
 };
--- hooks/useIsAuthenticated.ts
+++ hooks/useIsAuthenticated.ts
@@ -3,18 +3,28 @@ import { useEffect, useState } from 'react';
 
 interface IsAuthenticatedReturnValue {
   isAuthenticated: boolean;
-  userId?: string;
+  userId: string | null;
+}
+
+interface SessionUserData {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
 }
 
 export const useIsAuthenticated = (): IsAuthenticatedReturnValue => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const { status } = useSession();
+  const [userId, setUserId] = useState<string | null>(null);
+  const session = useSession();
 
   useEffect(() => {
-    setIsAuthenticated(status === 'authenticated');
-  }, [status]);
+    setIsAuthenticated(session.status === 'authenticated');
+    setUserId((session.data?.user as SessionUserData)?.id ?? null);
+  }, [session]);
 
   return {
     isAuthenticated,
+    userId,
   };
 };
