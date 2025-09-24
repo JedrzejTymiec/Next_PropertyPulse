4ffdc498fde967e104572efde4bba1c40efe11ff
Jedrzej Tymiec <jedrek1692@gmail.com>
2025-09-24 22:13:46 +0200

    feat: dynamic cache tags

--- src/queries/getProperty.ts
+++ src/queries/getProperty.ts
@@ -1,13 +1,13 @@
-import { unstable_cache } from 'next/cache';
 import { PropertyModel } from '@/models';
 import { connectDB } from '@/lib';
 import { CacheTag } from '@/constants/CacheTag';
+import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
+import { cacheLife } from 'next/dist/server/use-cache/cache-life';
 
-export const getProperty = unstable_cache(
-  async (id: string) => {
-    await connectDB();
-    return await PropertyModel.findById(id).lean();
-  },
-  [CacheTag.Property],
-  { revalidate: 86400 },
-);
+export const getProperty = async (id: string) => {
+  'use cache';
+  cacheTag(`${CacheTag.Property}:${id}`);
+  cacheLife('days');
+  await connectDB();
+  return await PropertyModel.findById(id).lean();
+};
